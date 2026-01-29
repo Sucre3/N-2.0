cec66209248bf79075795da0cf09b031a6736d7e
nty <natanaelciupe@gmail.com>
2026-01-29 19:37:07 +0200

    Initial Commit

---
+++ N 2.0/N_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  N_2_0App.swift
+//  N 2.0
+//
+//  Created by Nata on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct N_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
